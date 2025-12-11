a93bedb49103826a426e1b9fc5b012c4860ce3d6
rachelcheungg <rachelcheungusa@gmail.com>
2025-12-11 16:05:56 -0600

    created models for messages app

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="conversations_as_user1")
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="conversations_as_user2")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def participants(self):
+        return [self.user1, self.user2]
+
+    def __str__(self):
+        return f"Conversation between {self.user1} and {self.user2}"
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} at {self.timestamp}"
