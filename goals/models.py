46accc43e9f72ae57377a30c568da4926ad221a9
rachelcheungg <rachelcheungusa@gmail.com>
2025-12-11 10:09:53 -0600

    created model for financial tracker

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class FinancialTracker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="goals")
+    name = models.CharField(max_length=255)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    deadline = models.DateField(blank=True, null=True)
+
+    def progress_percentage(self):
+        if self.target_amount == 0:
+            return 0
+        return (self.current_amount / self.target_amount) * 100
+
+    def is_completed(self):
+        return self.current_amount >= self.target_amount
+
+    def __str__(self):
+        return self.name
