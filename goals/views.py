347cc74f9ec9e84b9c96068f75bdf56404a3f201
rachelcheungg <rachelcheungusa@gmail.com>
2025-12-11 10:40:09 -0600

    created views for financial tracker

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from . import models
+from .models import FinancialTracker
+from .forms import GoalForm, UpdateSavingsForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def goal_list(request):
+    filter_option = request.GET.get("filter", "all")
+
+    if filter_option == "completed":
+        goals = FinancialTracker.objects.filter(user=request.user, current_amount__gte=models.F('target_amount'))
+    elif filter_option == "in progress":
+        goals = FinancialTracker.objects.filter(user=request.user, current_amount__lt=models.F('target_amount'))
+    else:
+        goals = FinancialTracker.objects.filter(user=request.user)
+
+    return render(request, "goals/goal_list.html", {"goals": goals})
+
+@login_required
+def create_goal(request):
+    if request.method == "POST":
+        form = GoalForm(request.POST)
+        if form.is_valid():
+            goal = form.save(commit=False)
+            goal.user = request.user
+            goal.save()
+            return redirect("goal_list")
+    else:
+        form = GoalForm()
+    return render(request, "goals/create_goal.html", {"form": form})
+
+@login_required
+def update_savings(request, pk):
+    goal = get_object_or_404(FinancialTracker, pk=pk, user=request.user)
+    if request.method == "POST":
+        form = UpdateSavingsForm(request.POST, instance=goal)
+        if form.is_valid():
+            form.save()
+            return redirect("goal_list")
+    else:
+        form = UpdateSavingsForm(instance=goal)
+    return render(request, "goals/update_savings.html", {"form": form, "goal": goal})
